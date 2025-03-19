061e187d7f1b746b16f77afbb85aa8a4540820f7
Hannah Anderson <339729345@GoTVDSB.ca>
2025-03-19 14:39:02 -0400

    Starting the tasks

--- Topic 5 - If Statements-5.2/Program.cs
+++ Topic 5 - If Statements-5.2/Program.cs
@@ -4,7 +4,61 @@
     {
         static void Main(string[] args)
         {
-            
+            //int grade;
+            //Console.WriteLine("What was your grade?");
+            //int.TryParse(Console.ReadLine(), out grade);
+            //{
+            //    if (grade >= 50)
+            //        Console.WriteLine("You passed!");
+            //    else
+            //        Console.WriteLine("Better luck next time");
+            //}
+
+            //int age;
+            //Console.WriteLine("How old are you?");
+            //int.TryParse(Console.ReadLine(), out age);
+            //{
+            //    if (age >= 16)
+            //        Console.WriteLine("The roads are not safe!");
+            //    else
+            //        Console.WriteLine("I can drive without fear!");
+            //}
+
+            //int grade;
+            //Console.WriteLine("What was your grade?");
+            //int.TryParse(Console.ReadLine(), out grade);
+            //{
+            //    if (grade < 50)
+            //        Console.WriteLine("That is an F!");
+            //    else if (grade <= 65)
+            //        Console.WriteLine("That is a D!");
+            //    else if (grade <= 75)
+            //        Console.WriteLine("That is a C!");
+            //    else if (grade <= 85)
+            //        Console.WriteLine("That is a B!");
+            //    else
+            //        Console.WriteLine("That is a A!");
+            //}
+
+            //Console.WriteLine("Task one");
+            //int answer;
+            //Console.WriteLine("What is the answer to 13 + 12? Is it 23, 24, 25, or 26");
+            //int.TryParse(Console.ReadLine(), out answer);
+            //{
+            //    if (answer == 25)
+            //        Console.WriteLine("Correct!");
+            //    else if (answer >= 26)
+            //        Console.WriteLine("Incorrect.");
+            //    else if (answer <= 24)
+            //        Console.WriteLine("Incorrect.");
+            //    else
+            //        Console.WriteLine("Incorrect.");
+            }
+
+            //Console.WriteLine("Task two");
+
+
+            }
         }
     }
 }
